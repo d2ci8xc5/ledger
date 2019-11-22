4b2e98b39e0797f8faf29ee95eda151bce7b4f97
d2ci8xc5 <d2ci8xc5@tuta.io>
2019-11-22 17:44:46 +1300

    remove swp

--- src/account.rs
+++ src/account.rs
@@ -1,22 +1,37 @@
+use std::fmt;
+
 pub struct Account {
     name: String,
-    balance: u64,
-    id: i32,
+    pub balance: i32,
+    pub id: i32,
 }
 
 impl Account {
-    fn new(name: String, balance: u64, id: i32) -> Account {
-        let mut acc = Account { name, balance, id };
-        acc
+    fn new(name: String, balance: i32, id: i32) -> Result<Account, &'static str> {
+        return match balance {
+           b if b >= 0 => Ok(Account { name, balance, id }),
+           b if b < 0 => Err("invalid balance ( less than zero)"),
+           _ => Err("unknown error"),
+        }
     }
 }
 
+impl fmt::Display for Account{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "{} ({}): {}\n", self.id, self.name, self.id)
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_new() {
-        let acc = Account::new(String::from("a0"), 100: u64, 0: i32);
+    fn test_account_new() {
+        let account = Account::new(String::from("account_0"), 100u32, 0i32);
+        match account {
+            Ok(acc) => {},
+            Err(reason) => assert!(false), 
+            _ => assert!(false),
+        }
     }
 }
--- src/transaction.rs
+++ src/transaction.rs
@@ -1,10 +1,45 @@
 use crate::account::Account;
-
+use std::fmt;
 ///
 pub struct Transaction {
-    entry: Vec<(Account, i32)>,
+    pub entries: Vec<(Account, i32)>,
 }
 
 impl Transaction {
-    pub fn new() {}
+    pub fn new(entries: Vec<(Account, i32)>) -> Result<Transaction, &'static str> {
+        // Only allow legal transactions
+        let mut sumation : i32 = 0;
+        for entry in &entries {
+            if entry.0.balance + entry.1 < 0 {
+                return Err("Not enough funds"); 
+            }
+            sumation += entry.1;
+        }
+
+        if sumation != 0 {
+            return Err("Transaction is not balanced");
+        }
+
+        return Ok(Transaction{ entries });
+    }
+
+    ///Execute and commit the transaction to the ledger
+    pub fn complete(&mut self) {
+    }
+}
+
+impl fmt::Display for Transaction {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "")
+    }
+}
+
+
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_transaction_new() {
+    }
+
 }
